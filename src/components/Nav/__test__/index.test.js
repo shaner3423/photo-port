54c41b7665ca727a0cb4bf4fd9d0203c4f361958
Sugar Shane <scrisostomo17@gmail.com>
2021-10-16 00:21:32 -0400

    stuff

--- src/components/Nav/__test__/index.test.js
+++ src/components/Nav/__test__/index.test.js
@@ -3,17 +3,37 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..'
 
+const categories = [
+    { name: 'portraits', description: 'Portraits of people in my life' }
+  ]
+  const mockCurrentCategory = jest.fn();
+  const mockSetCurrentCategory = jest.fn();
+  const mockContactSelected = jest.fn();
+  const mockSetContactSelected = jest.fn();
+
 afterEach(cleanup);
 
 describe('Nav component', () => {
     //baseline test
     it('renders', () => {
-        render(<Nav />);
+        render(<Nav
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+            contactSelected={mockContactSelected}
+            setContactSelected={mockSetContactSelected}
+          />);
     })
 
     //snapshot test
     it('matches snapshot', () => {
-        const { asFragment } = render(<Nav />);
+        const { asFragment } = render(<Nav
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+            contactSelected={mockContactSelected}
+            setContactSelected={mockSetContactSelected}
+          />);
         //assert value comparison
         expect(asFragment()).toMatchSnapshot();
     });
